845d9346c7c9e2bed5ffa60c6b317afc462d858f
masahino <masahino@gmail.com>
2018-03-04 22:32:56 +0900

    add button and 7seg led display

--- gr_sketch.cpp
+++ gr_sketch.cpp
@@ -4,6 +4,8 @@
 #include "DHT11.h"
 #include "tkdn_usb.h"
 
+#include "TM1637.h"
+
 DHT11 *dht11;
 TEthernet Ethernet;
 EthernetServer server(80);
@@ -12,6 +14,12 @@ byte myip[] = {192, 168, 1, 223};
 static unsigned long start;
 static int client_status, client_timer;
 
+/* Button(P) */
+const int pinButton = 4;
+
+/* 7seg led */
+TM1637 tm1637(6, 7); /* D6 clock, D7 DIO */
+
 static void timer()
 {
   client_timer += 1;
@@ -19,9 +27,25 @@ static void timer()
     digitalWrite(PIN_LED2, HIGH);
     system_reboot(REBOOT_USERAPP);
   }
-  if(digitalRead(PIN_SW) == LOW){
+  if (digitalRead(PIN_SW) == LOW){
     system_reboot(REBOOT_USERAPP);
   }
+
+  if (digitalRead(pinButton)) {
+    system_reboot(REBOOT_USERAPP);
+  }
+}
+
+static void led_disp(int temp, int humid)
+{
+  int8_t NumTab[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+  int8_t ListDisp[4];
+
+  ListDisp[0] = temp / 10;
+  ListDisp[1] = temp % 10;
+  ListDisp[2] = humid / 10;
+  ListDisp[3] = humid % 10;
+  tm1637.display(ListDisp);
 }
 
 void setup()
@@ -31,10 +55,15 @@ void setup()
   pinMode(PIN_LED2, OUTPUT);
   pinMode(PIN_LED3, OUTPUT);
   pinMode(PIN_SW, INPUT);
+  pinMode(pinButton, INPUT);
   digitalWrite(PIN_LED0, HIGH);
   delay(1000);
   dht11 = new DHT11(2);
   dht11->begin();
+
+  /* 7seg init */
+  tm1637.init();
+  tm1637.set(BRIGHT_DARKEST);
 
   Serial.begin(9600);
   digitalWrite(PIN_LED0, LOW);
@@ -47,6 +76,13 @@ void setup()
   client_timer = 0;
   timer_regist_userfunc(timer);
 
+  /* for 7seg */
+  int8_t ListDisp[4];
+  for(int i = 0; i < 4; i++) {
+    ListDisp[i] = 0;
+  }
+  tm1637.display(ListDisp);
+
   digitalWrite(PIN_LED0, HIGH);
 }
 
@@ -56,6 +92,7 @@ void loop()
   float temp, humid;
   int   iret;
 
+  
   EthernetClient client = server.available();
   
   client_status = 0;
@@ -82,6 +119,7 @@ void loop()
           if ( iret == 0 ){
             digitalWrite(PIN_LED2, LOW);
             snprintf(msg,32,"T: %5.2f C, H: %5.2f %%", temp, humid);
+            led_disp((int)temp, (int)humid);
             if (TKUSB_IsConnected() == 1) {
               Serial.println(msg);
             }
